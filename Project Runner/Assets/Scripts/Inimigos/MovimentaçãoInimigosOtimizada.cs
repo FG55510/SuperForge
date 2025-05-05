955e52b0263c5857424bcf77e1c9da72b636024b
F.G.5510 <130413112+FG55510@users.noreply.github.com>
2025-05-05 16:46:53 -0300

    Update MovimentaçãoInimigosOtimizada.cs

--- Project Runner/Assets/Scripts/Inimigos/MovimentaçãoInimigosOtimizada.cs
+++ Project Runner/Assets/Scripts/Inimigos/MovimentaçãoInimigosOtimizada.cs
@@ -5,6 +5,8 @@ public class MovimentaçãoInimigosOtimizada : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private Animator anim;
     [SerializeField] private float velocidade = 3.0f;
+    [SerializeField] private float distancia;
+    
     public float rangeminpertodoplayer;
     public float rangemaxlongedoplayer;
     
@@ -26,33 +28,34 @@ public class MovimentaçãoInimigosOtimizada : MonoBehaviour
         
 
         if(!playernorangemin){
-        if (distancia > rangemaxlongedoplayer)
-        {
-
             Vector3 direcao = player.position - transform.position;
         
-            float distancia = direcao.magnitude;
-            
-            Vector3 movimento = direcao.normalized * velocidade;
-            rb.linearVelocity = new Vector3(movimento.x, rb.linearVelocity.y, movimento.z); 
-            direcao.y = 0f;
-
-           
-            Quaternion rotacaoAlvo = Quaternion.LookRotation(direcao);
-            rb.MoveRotation(Quaternion.Slerp(rb.rotation, rotacaoAlvo, 10f * Time.fixedDeltaTime));
-
-            anim.SetFloat("Speed", movimento.magnitude);
-        }
-        else
-        {
-            rb.linearVelocity = new Vector3(0f, rb.linearVelocity.y, 0f); 
-            anim.SetFloat("Speed", 0f);
-        }
-        }
+            distancia = direcao.magnitude;
+            if (distancia > rangemaxlongedoplayer)
+            {
+    
+                
+                
+                Vector3 movimento = direcao.normalized * velocidade;
+                rb.linearVelocity = new Vector3(movimento.x, rb.linearVelocity.y, movimento.z); 
+                direcao.y = 0f;
+    
+               
+                Quaternion rotacaoAlvo = Quaternion.LookRotation(direcao);
+                rb.MoveRotation(Quaternion.Slerp(rb.rotation, rotacaoAlvo, 10f * Time.fixedDeltaTime));
+    
+                anim.SetFloat("Speed", movimento.magnitude);
+            }
+            else
+            {
+                rb.linearVelocity = new Vector3(0f, rb.linearVelocity.y, 0f); 
+                anim.SetFloat("Speed", 0f);
+            }
+            }
         else{
-        Vector3 direcao = transform.position - player.position;
+            Vector3 direcao = transform.position - player.position;
         
-            float distancia = direcao.magnitude;
+            distancia = direcao.magnitude;
             Vector3 movimento = direcao.normalized * velocidade;
             rb.linearVelocity = new Vector3(movimento.x, rb.linearVelocity.y, movimento.z); 
             direcao.y = 0f;
